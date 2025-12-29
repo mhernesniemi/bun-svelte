5bcde343076195deec8f46126c660f5ee6a14349
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2025-12-29 08:05:01 +0200

    optimise sqlite

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -6,5 +6,7 @@ import { env } from "$env/dynamic/private";
 if (!env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
 
 const client = new Database(env.DATABASE_URL);
+client.run("PRAGMA journal_mode = WAL");
+client.run("PRAGMA synchronous = NORMAL");
 
 export const db = drizzle(client, { schema });
