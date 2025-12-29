f39643030d326e061b44e4568c5820cf1963b2b5
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2025-12-29 14:05:57 +0200

    add view transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,20 @@
   import "./layout.css";
   import favicon from "$lib/assets/favicon.svg";
   import { Toaster } from "$lib/components/ui/sonner";
+  import { onNavigate } from "$app/navigation";
 
   let { children } = $props();
+
+  onNavigate((navigation) => {
+    if (!document.startViewTransition) return;
+
+    return new Promise((resolve) => {
+      document.startViewTransition(async () => {
+        resolve();
+        await navigation.complete;
+      });
+    });
+  });
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /><title>Feedback Tool</title></svelte:head>
